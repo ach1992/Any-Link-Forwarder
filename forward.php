e921f5626dc9f243b9bebb149a2e7857d443ad0f
Ali Chopani <58507994+ach1992@users.noreply.github.com>
2025-07-29 21:46:25 +0330

    Update forward.php

--- forward.php
+++ forward.php
@@ -1,9 +1,7 @@
 <?php
 
-if (empty($_SERVER['HTTP_USER_AGENT'])) {
-    http_response_code(403);
-    exit("Access Denied");
-}
+header_remove('X-Powered-By');
+header_remove('Connection');
 
 $accept = $_SERVER['HTTP_ACCEPT'] ?? '';
 $isTextHTML = str_contains($accept, 'text/html') || str_contains($accept, '*/*');
@@ -27,13 +25,15 @@ $portSegment = ($targetPort == 443) ? '' : ':' . $targetPort;
 $URL = "https://{$targetDomain}{$portSegment}{$proxyPath}";
 
 $ch = curl_init();
+curl_setopt($ch, CURLOPT_ENCODING, '');
 curl_setopt($ch, CURLOPT_URL, $URL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-curl_setopt($ch, CURLOPT_HTTPHEADER, $isTextHTML ? ['Accept: text/html'] : []);
 
 $data = curl_exec($ch);
 $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -50,14 +50,11 @@ $body = substr($data, strlen($header_text) + 4);
 
 foreach (explode("\r\n", $header_text) as $i => $line) {
     if ($i === 0) continue;
-    list($key, $value) = explode(': ', $line, 2);
-    $key = strtolower($key);
-    if (in_array($key, ['subscription-userinfo', 'profile-update-interval'])) {
-        header(ucwords($key, '-') . ': ' . trim($value));
-    }
+    if (stripos($line, 'Content-Length') === 0 || stripos($line, 'Transfer-Encoding') === 0 || stripos($line, 'Connection') === 0 || stripos($line, 'Content-Encoding') === 0 || stripos($line, 'Date') === 0 || stripos($line, 'Server') === 0) continue;
+    header($line);
 }
 
-header('Content-Type: text/plain');
-
 http_response_code($code);
+header('Connection: keep-alive');
+header('Content-Length: ' . strlen($body));
 echo $body;
